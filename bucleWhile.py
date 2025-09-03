c81f94b0d31cd892720f52a4e57e558a5a5e7234
Diego Aguirre <vfxartist.diegoaguirre@gmail.com>
2025-09-03 11:30:24 -0300

    while
    added code
    super basic code for a while

---
+++ bucleWhile.py
@@ -0,0 +1,13 @@
+# una cosa que tiene que quedar en claro, es que 
+# while true:
+# break
+# esto no! solo en un ejemplo de la hora. 
+
+#hasta que la condicion deja de ser verdadera, se sigue repitiendo
+
+#cuando no sabemos hasta cuando cortar, usamos el while. 
+i = 0
+while i < 5:
+    print("Hello world")
+    i = i + 1
+    
